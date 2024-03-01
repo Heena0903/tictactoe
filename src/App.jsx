37c3f62c55f837243edc7e06d00e234962c1a6e0
Heena Sharma <sharmahemlata0903@gmail.com>
2024-03-01 17:19:34 +0530

    added reset button

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,9 @@ import History from "./components/History";
 import { calculateWinner } from "./winner";
 
 import "./styles.scss";
-
+const NEW_GAME = [{ squares: Array(9).fill(null), isXNext: false }];
 function App() {
-  const [history, setHistory] = useState([
-    { squares: Array(9).fill(null), isXNext: false },
-  ]);
+  const [history, setHistory] = useState(NEW_GAME);
 
   const [currentMove, setCurrentMove] = useState(0);
 
@@ -56,6 +54,10 @@ function App() {
   const moveTo = (move) => {
     setCurrentMove(move);
   };
+  const onNewGameStart = () => {
+    setHistory(NEW_GAME);
+    setCurrentMove(0);
+  };
   return (
     <div className="app">
       <StatusMessage winner={winner} gamingBoard={gamingBoard} />
@@ -63,6 +65,14 @@ function App() {
         squares={gamingBoard.squares}
         handleSquareClick={handleSquareClick}
       />
+
+      <button
+        type="button"
+        onClick={onNewGameStart}
+        className={`btn-reset ${winner ? "active" : ""}`}
+      >
+        Start new Game
+      </button>
 
       <h2>Current Game History</h2>
       <History history={history} moveTo={moveTo} CurrentMove={currentMove} />
